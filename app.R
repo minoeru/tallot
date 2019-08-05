39fae2d8e0a5d9d3659efe00135645e58a65376e
minoeru <minoeru@users.noreply.github.com>
2019-08-05 20:38:45 +0900

    no message

--- app.R
+++ app.R
@@ -2,8 +2,9 @@ library(shiny)
 library(fmsb)
 library(shinythemes)
 
+#csv読み込み
 df <- read.csv("tallot.csv")
-
+#csv各値を変数に
 df_id <- df$id
 df_a <- df$para_a
 df_b <- df$para_b
@@ -12,141 +13,156 @@ df_d <- df$para_d
 df_e <- df$para_e
 df_sentence <- as.character(df$sentence)
 df_sentence2 <- as.character(df$sentence2)
-
-
+#最大、最小データの準備
+maxmin <- data.frame(
+  para_a = c(100, 0),
+  para_b = c(100, 0),
+  para_c = c(100, 0),
+  para_d = c(100, 0),
+  para_e = c(100, 0)
+)
+#カード指定の変数
 num <- 1
 
 ui <- fluidPage(
-  theme = shinytheme("cyborg"),#黒画面
+  #黒画面
+  theme = shinytheme("cyborg"),
   
   titlePanel("タロットゲーム"),
-  tabsetPanel(type = "tabs",
-              
-              tabPanel("game",
-                       
-                       # fluidRow(
-                       #   column(2,uiOutput("fuga1")),
-                       #   column(2,uiOutput("fuga2")),
-                       #   column(2,uiOutput("fuga3")),
-                       #   column(2,uiOutput("fuga4")),
-                       #   column(2,uiOutput("fuga5")),
-                       #   column(2,uiOutput("fuga6"))
-                       # ),
-                       # fluidRow(
-                       #   column(1),
-                       #   column(2,uiOutput("fuga7")),
-                       #   column(2,uiOutput("fuga8")),
-                       #   column(2,uiOutput("fuga9")),
-                       #   column(2,uiOutput("fuga10")),
-                       #   column(3,uiOutput("fuga11"))
-                       # )
-                       
-                       
+  tabsetPanel(type = "tabs",id="tallot_tab",selected = "Start",
+              tabPanel(title = "Start",value = "panel_1",
+                       actionButton("start_button","Start"),
+                       actionButton("staff_button","Staff_list")
+              ),
+              tabPanel(title = "Game",value = "panel_2",
                        fluidRow(
-                         column(5),
-                         column(1,uiOutput("fuga1")),
+                         column(5),column(1,uiOutput("fuga1")),
                          column(6)
                        ),
                        fluidRow(
-                         column(3),
-                         column(1,uiOutput("fuga2")),
-                         column(3),
-                         column(1,uiOutput("fuga3")),
+                         column(3),column(1,uiOutput("fuga2")),
+                         column(3),column(1,uiOutput("fuga3")),
                          column(4)
                        ),
                        fluidRow(
-                         column(2),
-                         column(1,uiOutput("fuga4")),
-                         column(5),
-                         column(1,uiOutput("fuga5")),
+                         column(2),column(1,uiOutput("fuga4")),
+                         column(5),column(1,uiOutput("fuga5")),
                          column(3)
                        ),
                        fluidRow(
-                         column(1),
-                         column(1,uiOutput("fuga6")),
-                         column(3),
-                         column(1,uiOutput("fuga7")),
-                         column(3),
-                         column(1,uiOutput("fuga8")),
+                         column(1), column(1,uiOutput("fuga6")),
+                         column(3),column(1,uiOutput("fuga7")),
+                         column(3),column(1,uiOutput("fuga8")),
                          column(2)
                        ),
                        fluidRow(
-                         column(2),
-                         column(1,uiOutput("fuga9")),
-                         column(5),
-                         column(1,uiOutput("fuga10")),
+                         column(2),column(1,uiOutput("fuga9")),
+                         column(5),column(1,uiOutput("fuga10")),
                          column(3)
                        ),
                        fluidRow(
-                         column(3),
-                         column(1,uiOutput("fuga11")),
-                         column(3),
-                         column(1,uiOutput("fuga12")),
+                         column(3),column(1,uiOutput("fuga11")),
+                         column(3),column(1,uiOutput("fuga12")),
                          column(4)
                        ),
                        fluidRow(
-                         column(5),
-                         column(1,uiOutput("fuga13")),
+                         column(5),column(1,uiOutput("fuga13")),
                          column(6)
                        )
-                       
-                       
-                       
               ),
-              tabPanel("結果",
+              tabPanel(title = "Result",value = "panel_3",
                        sidebarLayout(
                          sidebarPanel(
-                           uiOutput("sentence"),
-                           textOutput("sentence2")
+                           plotOutput("radarPlot")
                          ),
                          mainPanel(
-                           plotOutput("radarPlot")
+                           uiOutput("sentence"),
+                           uiOutput("sentence2"),
+                           uiOutput("tweet"),
+                           fluidRow(
+                             column(9),column(3,actionButton("Back_to_start_button1","Back"))
+                           )
                          )
+                       )
+              ),
+              tabPanel(title = "Staff List",value="panel_4",
+                       h2("Illustrator", align = "center"),
+                       h4("ロジ まつり",align="center"),
+                       h4("かずみ サ^ｎ ろろ オダマキ れしぃ やまろ fuchi ゆーが okanon ここなつ ",align="center"),
+                       h4("てぃらみー エディ ごぼぬん 藤汐 aozam サカモト ", align = "center"),
+                       h2("Designer", align = "center"),
+                       h4("okanon fuchi", align = "center"),
+                       h2("Programmer", align = "center"),
+                       h4("ミノエル れしぃ comane fuchi", align = "center"),
+                       h2("Tea lady(Boss)", align = "center"),
+                       h4("葵", align = "center"),
+                       fluidRow(
+                         column(11),column(1,actionButton("Back_to_start_button2","Back"))
                        )
               )
   )
 )
 
-server <- function(input, output) {
+server <- function(input, output,session) {
   
-  
-  #最大、最小データの準備
-  maxmin <- data.frame(
-    para_a = c(100, 0),
-    para_b = c(100, 0),
-    para_c = c(100, 0),
-    para_d = c(100, 0),
-    para_e = c(100, 0)
+  #スタート画面に飛ばす
+  updateTabsetPanel(session, "tallot_tab",
+                    selected = paste("panel_", 1,sep = "")
   )
   
+  ##################  スタート画面   ########################
+  #スタートボタンを押した際にゲーム画面に遷移
+  observeEvent(input$start_button, {
+    updateTabsetPanel(session, "tallot_tab",
+                      selected = paste("panel_", 2,sep = "")
+    )
+    all_reverse()
+  })
+  #スタッフボタンを押した際にスタッフ画面に遷移
+  observeEvent(input$staff_button, {
+    updateTabsetPanel(session, "tallot_tab",
+                      selected = paste("panel_", 4,sep = "")
+    )
+  })
   
+  ##################  ゲーム画面   ########################
+  #全てのカードを裏返す関数
+  all_reverse <- function(){
+    output$fuga1 <- renderUI({make_ui(1)})
+    output$fuga2 <- renderUI({make_ui(2)})
+    output$fuga3 <- renderUI({make_ui(3)})
+    output$fuga4 <- renderUI({make_ui(4)})
+    output$fuga5 <- renderUI({make_ui(5)})
+    output$fuga6 <- renderUI({make_ui(6)})
+    output$fuga7 <- renderUI({make_ui(7)})
+    output$fuga8 <- renderUI({make_ui(8)})
+    output$fuga9 <- renderUI({make_ui(9)})
+    output$fuga10 <- renderUI({make_ui(10)})
+    output$fuga11 <- renderUI({make_ui(11)})
+    output$fuga12 <- renderUI({make_ui(12)})
+    output$fuga13 <- renderUI({make_ui(13)})
+    
+  }
   
-  ##########################################
-  output$fuga1 <- renderUI({make_ui(1)})
-  output$fuga2 <- renderUI({make_ui(2)})
-  output$fuga3 <- renderUI({make_ui(3)})
-  output$fuga4 <- renderUI({make_ui(4)})
-  output$fuga5 <- renderUI({make_ui(5)})
-  output$fuga6 <- renderUI({make_ui(6)})
-  output$fuga7 <- renderUI({make_ui(7)})
-  output$fuga8 <- renderUI({make_ui(8)})
-  output$fuga9 <- renderUI({make_ui(9)})
-  output$fuga10 <- renderUI({make_ui(10)})
-  output$fuga11 <- renderUI({make_ui(11)})
-  output$fuga12 <- renderUI({make_ui(12)})
-  output$fuga13 <- renderUI({make_ui(13)})
+  all_reverse()
   
+  #カードを裏返す関数
   make_ui <- function(x){
     text = paste("button",x,sep = "")
     tags$button(
       id = text,
       class = "btn action-button",
-      tags$img(src = paste("cc_",0,".jpg",sep = ""),
+      tags$img(src = paste("cc_","00",".jpg",sep = ""),
                height = "300px",width = "150px"
       )
+      #実際はこれ
+      # tags$img(src = paste("tallot_",00,".jpg",sep = ""),
+      #          height = "300px",width = "150px"
+      # )
+      
     )
   }
-  
+  #各カードのボタンが押された際に乱数生成=>画像変換=>結果描写
   observeEvent(input$button1,{
     ran_make()
     output$fuga1 <- renderUI({turn_card(1)})
@@ -212,12 +228,14 @@ server <- function(input, output) {
     output$fuga13 <- renderUI({turn_card(13)})
     change()
   })
-  
+  #カード選択用乱数生成関数
   ran_make <- function(){
-    x <- runif(1,1,20)
-    num <<- floor(x)
+    x <- floor(runif(1,0,20))#実際は1,1,23
+    ran <- floor(runif(1,1,3))
+    ifelse(ran == 1,num <<- x,num <<-x + 100)
+    
   }
-  
+  #カードを裏返す関数
   turn_card <- function(x){
     text = paste("button",x,sep="")
     tags$button(
@@ -230,19 +248,21 @@ server <- function(input, output) {
     )
   }
   
+  ##################  結果画面   ########################
+  #結果画面のレーダーチャートとその他を作る関数
   change <- function(){
     
     #描写データの準備
     dat <- data.frame(
-      para_a = df_a[num],
-      para_b = df_b[num],
-      para_c = df_c[num],
-      para_d = df_d[num],
-      para_e = df_e[num]
+      para_a = df_a[df$id == num],
+      para_b = df_b[df$id == num],
+      para_c = df_c[df$id == num],
+      para_d = df_d[df$id == num],
+      para_e = df_e[df$id == num]
     )
     dat <- rbind(maxmin, dat) #データの結合
-    VLabel <- c("a","b","c","d","e") #ラベルの名前
-    
+    VLabel <- c("a","b","c","d","e") #ラベルの名前！！！（決まり次第ここを変える）
+    #レーダーチャート作成
     output$radarPlot <- renderPlot({
       radarchart(dat, 
                  axistype = 0,#ラベル表示無し
@@ -258,19 +278,38 @@ server <- function(input, output) {
                  pangle=180,　#塗りつぶす斜線の傾き
                  pfcol=7,　#塗りつぶす色(黄色)
                  cglcol="pink",#軸の色
-                 title = "Luck")
+                 # title = "Luck"
+                 title = df_sentence[df$id == num]
+      )
       
     })
     output$sentence <- renderUI({
-      h1(df_sentence[num])
+      h1(df_sentence[df$id == num])
     })
-    output$sentence2 <- renderText({
-      df_sentence2[num]
+    output$sentence2 <- renderUI({
+      h5(df_sentence2[df$id == num])
+    })
+    output$tweet <- renderUI({
+      actionButton("Tweet_button","Tweet",onclick = "window.open('https://twitter.com/login?lang=ja', '_blank')")
     })
   }
+  #Backボタンが押された際にカードを裏返してStart画面へ
+  observeEvent(input$Back_to_start_button1, {
+    updateTabsetPanel(session, "tallot_tab",
+                      selected = paste("panel_", 1,sep = "")
+    )
+    all_reverse()
+  })
+  
+  
+  ##################  スタッフ画面   ########################
+  #Backボタンが押された際にStart画面へ
+  observeEvent(input$Back_to_start_button2, {
+    updateTabsetPanel(session, "tallot_tab",
+                      selected = paste("panel_", 1,sep = "")
+    )
+  })
 }
-
 shinyApp(ui = ui, server = server)
-
 
 
